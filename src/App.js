dc432183e6515eef2066f5750b1268985f1ef2d7
Jay Shrivastava <jay13102000@gmail.com>
2020-05-27 00:42:29 +0530

    Adding logo nad background

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import FlashcardList from './FlashcardList';
 import './App.css'
 import axios from 'axios'
+import logo from './test.png'
 
 function App() {
   const [flashcards, setFlashcards] = useState(Sample)
@@ -56,26 +57,34 @@ function App() {
 
   return (
     <>
-      <form className="header" onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="category">Category</label>
-          <select id="category" ref={categoryEl}>
-            {categories.map(category => {
-              return <option value={category.id} key={category.id}>{category.name}</option>
-            })}
-          </select>
+      <form className="header row" onSubmit={handleSubmit}>
+          <div className="row logo">
+            <div className="col-12 col-md">
+              <center> <img src={logo} height="120px" /> </center>
+            </div>
+          </div>
+         
+          <div className="form-group form-control">
+            <label htmlFor="category"><strong>Category</strong></label>
+            <select id="category" ref={categoryEl}>
+              {categories.map(category => {
+                return <option value={category.id} key={category.id}>{category.name}</option>
+              })}
+            </select>
+          </div>
+          <div className="form-group form-control">
+            <label htmlFor="amount"><strong>Number of Questions</strong></label>
+            <input type="number" id="amount" min="2" max="50" step="1" defaultValue={10} ref={amountEl} />
+          </div>
+          <div className="form-group form-control">
+            <button className="btn btn-primary">Generate</button>
+          </div>
+        </form>
+    
+
+        <div className="container">
+          <FlashcardList flashcards={flashcards} />
         </div>
-        <div className="form-group">
-          <label htmlFor="amount">Number of Questions</label>
-          <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl} />
-        </div>
-        <div className="form-group">
-          <button className="btn">Generate</button>
-        </div>
-      </form>
-      <div className="container">
-        <FlashcardList flashcards={flashcards} />
-      </div>
     </>
   );
 }
@@ -83,10 +92,10 @@ function App() {
 const Sample = [
   {
     id: 1,
-    question: 'This is a Sample Question, Please tap on the flashcard to check the answer ',
-    answer: 'Correct Answer',
+    question: `This is a Sample Question, Please tap on the flashcard to check the answer:------------------------ What's my Roll no. ? `,
+    answer: '18CS04 . Great!, Now Select the category you want and start testing your knowledge.',
     options:[
-      'Option 1', 'Option 2', 'Option 3', 'Option 4'
+      '18CS04', '18CSE', '18CS', '18CS4'
     ]
   }
 ]
